508b2e08a417c7557d4c9307a498bb5d88329f74
jojofave <ozonahebere@gmail.com>
2022-12-21 14:09:03 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,31 +6,31 @@
  *
  * Return: A pointer to the changed string
  */
-char *cap_string(char *);
+char *cap_string(char *)
 {
 	int index = 0;
 
 	while (str[index])
 	{
-	       while (!(str[index] >= 'a' && str[index] <= 'z'))
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
 		   index++;
 
-	   if (str[index -1] == ' '||
-	       str[index -1] == '\t' ||
-	       str[index -1] == '\n' ||
-	       str[index -1] == ',' ||
-	       str[index -1] == ';' ||
-	       str[index -1] == '.' ||
-	       str[index -1] == '!' ||
-	       str[index -1] == '?' ||
-	       str[index -1} == '"' ||
-	       str[index -1] == '(' ||
-	       str[index -1] == ')' ||
-	       str[index -1] == '{' ||
-	       str[index -1] == '}' ||
-	       index == 0)
+	if (str[index - 1] == ' ' ||
+	       str[index - 1] == '\t' ||
+	       str[index - 1] == '\n' ||
+	       str[index - 1] == ',' ||
+	       str[index - 1] == ';' ||
+	       str[index - 1] == '.' ||
+	       str[index - 1] == '!' ||
+	       str[index - 1] == '?' ||
+	       str[index - 1] == '"' ||
+	       str[index - 1] == '(' ||
+	       str[index - 1] == ')' ||
+	       str[index - 1] == '{' ||
+	       str[index - 1] == '}' ||
+	       index == 0
 		   str[index] -= 32;
-              index++;
-}      
-       return (str);
-       }
+index++;
+}
+return (s);
+}
