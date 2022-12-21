12c12c890c31b71ffd171ae1354280476a40d4ae
jojofave <ozonahebere@gmail.com>
2022-12-21 12:57:34 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,17 @@
  */
 char *_strcat(char *dest, const char *src)
 {
-	 int index = 0, dest_len = 0;
-while (dest[index++])
-dest_len++;
+	 int length, j;
 
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-
-return (dest);
+	 length = 0;
+	 while (dest[length] != '\0')
+	 {
+	length++;
+	 }
+	 for (j = 0; src[j] != '\0'; j++ length++)
+	 {
+	dest[length] = src[j];
+	 }
+	 dest[length] = '\0';
+	 return (dest);
 }
