d7d2dabcac4fa9ebfcd30d21260bfd64b081975a
jojofave <ozonahebere@gmail.com>
2022-12-21 14:21:51 -0800

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,20 +6,21 @@
  *
  * Return: A pointer to the encoded string
  */
-char *leet(char *)
+char *leet(char *s)
 {
 	int indx1 = 0, indx2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[indx1])
+	while (s[indx1])
 	{
 		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-			if (str[indx1] == leet[indx2] ||
-				str[indx1] - 32 == leet[indx2])
-				str[indx1] = indx2 + '0'
+			if (s[indx1] == leet[indx2] ||
+				s[indx1] - 32 == leet[indx2])
+				s[indx1] = indx2 + '0';
 		}
+
 		indx1++;
 	}
-	return (str);
+	return (s);
 }
