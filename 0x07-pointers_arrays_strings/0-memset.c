03868cdf72f824f5edfd344118d1ef82349b7358
jojofave <ozonahebere@gmail.com>
2023-01-02 03:33:58 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,10 +8,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i = 0
 
-	for (i = 0; i < n; i++)
- *(s + i) = b;
-
+	for (; n > 0; i++)
+	{
+		s[i] = b
+		n--;
+	}
 	return (s);
 }
