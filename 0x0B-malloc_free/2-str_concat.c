abc73d6f60121aeafcaf4fef583482809833f437
jojofave <ozonahebere@gmail.com>
2023-01-09 00:52:13 -0800

    concar

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Main entry
+ * @width: input
+ * @height: input
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2) /*don't want semi colon here*/
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+				free(grid[i--]);
+			free(grid);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+
+	}
+	return (grid);
+}
