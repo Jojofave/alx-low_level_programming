08bd94202785742526fd8a7dff9b1b7a8a6e7a95
jojofave <ozonahebere@gmail.com>
2023-01-11 23:50:46 -0800

    array reange

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - Creates an array of integers ordered
+ *               from min to max, inclusive.
+ * @min: The first value of the array.
+ * @max: The last value of the array.
+ *
+ * Retrun: array
+ */
+int *array_range(int min, int max) /*don't want semi colon*/
+{
+	int *arr, i = 0, t = min;
+
+	if (min > max)
+		return (0);
+	arr = malloc((max - min + 1) * sizeof(int));
+
+	if (!arr)
+		return (0);
+	while (i <= max - min)
+		arr[i++] = t++;
+	return (arr);
+}
