25b9d6153952d749cb50123d7b7e2d6b1aebc1a6
jojofave <ozonahebere@gmail.com>
2023-01-26 01:39:25 -0800

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ */
+void free_list(list_t *head) /*don't want semi colon*/
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
