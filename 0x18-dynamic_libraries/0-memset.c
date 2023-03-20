ec4264e6f379e61915787e3a0a3d45f15c55978b
jojofave <ozonahebere@gmail.com>
2023-03-20 10:00:41 -0700

    dynamics

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+;{
+	int i = 0
+
+	;for (; n > 0; i++;)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (void);
+;{
---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * strcat - Concatenates the string pointed to by @src,including the x
+ *          null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	 int length, j;
+
+	 length = 0;
+	 while (dest[length] != '\0')
+	 {
+	length++;
+	 }
+	 for (j = 0; src[j] != '\0'; j++) length++;
+	 {
+	dest[length] = src[j];
+	 }
+	 dest[length] = '\0';
+	 return (dest);
+}
